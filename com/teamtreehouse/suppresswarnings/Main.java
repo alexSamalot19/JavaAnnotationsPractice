2f9c99311db47995ae6c942aff8eeece71f0d9be
alexSam <alexsamalot19@gmail.com>
2020-03-14 16:46:38 -0400

    resolving warnings without suppressing

--- com/teamtreehouse/suppresswarnings/Main.java
+++ com/teamtreehouse/suppresswarnings/Main.java
@@ -4,22 +4,21 @@ import java.util.List;
 import java.io.Console;
 
 public class Main {
-    @SuppressWarnings({"static", "fallthrough"})
+
     public static void main(String[] args) {
         Console console = System.console();
         
         // Get number of posts
         int numPosts = Integer.parseInt(console.readLine("How many posts? "));
         
-        Blog b = new Blog();
-        
         // Fetch latest posts
-        List<Post> posts = b.getLatestPosts(numPosts);
+        List<Post> posts = Blog.getLatestPosts(numPosts);
         
         switch(numPosts) {
         case 0:
             // Display message
             System.out.println("No posts");
+            break;
         case 1:
             // Display post
             System.out.println(posts.get(0));
