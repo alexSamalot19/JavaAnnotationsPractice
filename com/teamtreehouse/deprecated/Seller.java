5448eca9bb3cd9947ebc69812470085c9e3417b3
alexSam <alexsamalot19@gmail.com>
2020-03-14 15:06:46 -0400

    Override and Deprecated annotations provide relevant error messages at compilation

--- com/teamtreehouse/deprecated/Seller.java
+++ com/teamtreehouse/deprecated/Seller.java
@@ -1,8 +1,16 @@
 package com.teamtreehouse.deprecated;
 
+import java.util.List;
+import java.awt.Image;
+
+
 public class Seller {
-    
+    @Deprecated
     public void placeAdInNewspaper(String adText, int numDays) {
         // Do all of the old school things
+    }
+
+    public void postInCraigslist(String adText, List<Image> image){
+
     }
 }
--- com/teamtreehouse/override/Cheese.java
+++ com/teamtreehouse/override/Cheese.java
@@ -1,6 +1,8 @@
 package com.teamtreehouse.override;
 
 public class Cheese{
+
+    @Override
     public String toString(String something){
         return "String Cheese";
     }
